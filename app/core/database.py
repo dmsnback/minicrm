6782149466deffdbcffd95a2061d04c1f5b955d5
Dmitry Titenkov <lt200711@yandex.ru>
2025-12-04 20:04:19 +0300

    id и tablename создается автоматически

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
 from app.core.config import settings
 
@@ -10,6 +10,16 @@ sync_session = async_sessionmaker(engine, expire_on_commit=False)
 
 class Model(DeclarativeBase):
     pass
+
+
+class PreBase(Model):
+    __abstract__ == True # type: ignore
+
+    @declared_attr
+    def __tablename__(cls)->str:
+        return cls.__name__.lower()
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
 
 
 async def get_session():
